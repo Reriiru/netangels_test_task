0f5d3d974c3bd0154823bd474e284755bd37eac0
ivelme <ivelme@yandex.ru>
2017-11-24 10:15:14 +0500

    fixed validation on URL

--- test_task/test_task_app/forms.py
+++ test_task/test_task_app/forms.py
@@ -2,12 +2,19 @@ import uuid
 
 from django.forms import ModelForm
 from test_task_app.models import ShortenedUrl
+from django.core.validators import URLValidator
 
 
 class ShortenerForm(ModelForm):
     class Meta:
         model = ShortenedUrl
         fields = ['original_url']
+    
+    def clean_original_url(self):
+        original_url = self.cleaned_data.get('original_url')
+        validate = URLValidator()
+        validate(original_url)
+        return original_url
 
     def save(self):
         media = super(ShortenerForm, self).save(commit=False)
--- test_task/test_task_app/models.py
+++ test_task/test_task_app/models.py
@@ -5,7 +5,7 @@ from django.core.validators import URLValidator
 
 
 class ShortenedUrl(models.Model):
-    original_url = models.TextField(max_length=2100, validators=[URLValidator])
+    original_url = models.TextField(max_length=2100)
     shortened_uri = models.CharField(max_length=20, unique=True)
     hits = models.IntegerField(default=0)
     creation_date = models.DateField(default=datetime.date.today)
